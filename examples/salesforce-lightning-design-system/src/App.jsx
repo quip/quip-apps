1513a186ba804a245a3959c7517d08eaa61143d8
Lindsey Simon <lindsey@quip.com>
2018-09-10 14:29:34 -0700

    utility sprite now working

--- examples/salesforce-lightning-design-system/src/App.jsx
+++ examples/salesforce-lightning-design-system/src/App.jsx
@@ -3,13 +3,7 @@ import "./App.css";
 
 import {Card, IconSettings} from "@salesforce/design-system-react";
 
-//import actionSprite from "@salesforce-ux/design-system/assets/icons/action-sprite/svg/symbols.svg";
-//import customSprite from "@salesforce-ux/design-system/assets/icons/custom-sprite/svg/symbols.svg";
-//import utilitySprite from "@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg";
-
-//import doctypeSprite from "@salesforce-ux/design-system/assets/icons/doctype-sprite/svg/symbols.svg";
-
-import StandardSprite from "@salesforce-ux/design-system/assets/icons/standard-sprite/svg/symbols.svg";
+import UtilitySprite from "@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg";
 
 import Buttons from "./Buttons.jsx";
 
@@ -17,20 +11,13 @@ export default class App extends React.Component {
     render() {
         return (
             <IconSettings
-                iconPath={(id, type) => {return Sta;}}
                 standardSprite={""}
                 utilitySprite={""}
                 actionSprite={""}
                 doctypeSprite={""}
                 customSprite={""}>
                 <div className="App">
-                    <div>
-                        <Card heading={quiptext("SVG")}>
-                            <div className="CardContent">
-                                <StandardSprite id="SVG" />
-                            </div>
-                        </Card>
-                    </div>
+                    <UtilitySprite />
                     <Card heading={quiptext("Buttons")}>
                         <div className="CardContent">
                             <Buttons />
