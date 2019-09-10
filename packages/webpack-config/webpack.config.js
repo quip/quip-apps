5a0228f0608fdeb94efe0c11d65648da924a7618
Jesse Ditson <jesse.ditson@gmail.com>
2019-09-10 13:14:16 -0700

    More fixes toward v1
    Rely on babel-env to include core-js
    Add workaround for https://github.com/quip/quip-apps/issues/47

--- packages/webpack-config/webpack.config.js
+++ packages/webpack-config/webpack.config.js
@@ -1,7 +1,6 @@
 // Copyright 2018 Quip
 
 const fs = require("fs");
-const webpack = require("webpack");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
@@ -13,8 +12,11 @@ const devMode = process.env.NODE_ENV === "development";
 
 function babelrcOptions() {
     const babelrcPath = path.resolve(process.cwd(), ".babelrc");
+    const babelConfigPath = path.resolve(process.cwd(), "babel.config.js");
     if (fs.existsSync(babelrcPath)) {
         return JSON.parse(fs.readFileSync(babelrcPath));
+    } else if (fs.existsSync(babelConfigPath)) {
+        return require(babelConfigPath);
     }
     return {presets: ["@babel/preset-env", "@babel/preset-react"]};
 }
@@ -25,13 +27,9 @@ function minimizers() {
         minimizers = [
             new TerserPlugin({
                 terserOptions: {
-                    mangle: {
-                        properties: {
-                            reserved: ["quip-text"],
-                        },
-                    },
+                    mangle: false,
                 },
-            });,
+            }),
             new OptimizeCSSAssetsPlugin({}),
         ];
     }
@@ -41,11 +39,7 @@ function minimizers() {
 module.exports = {
     devtool: "source-map",
     mode: devMode ? "development" : "production",
-    entry: [
-        "core-js/stable",
-        "regenerator-runtime/runtime",
-        path.resolve(cwd, "./src/root.jsx"),
-    ],
+    entry: ["regenerator-runtime/runtime", path.resolve(cwd, "./src/root.jsx")],
     output: {
         path: path.resolve(cwd, "./app/dist"),
         filename: "app.js",
