2c0933a45f432bde5d82d29c445f1c884b95ffea
Jesse Ditson <jditson@quip.com>
2020-07-10 14:37:04 -0700

    change migrations fixtures folder, fail tests if it's dirty, add one more test

--- packages/quip-cli/test/migration.test.ts
+++ packages/quip-cli/test/migration.test.ts
@@ -9,6 +9,13 @@ const exec = util.promisify(exec_node);
 describe("qla migration", () => {
     const useFixtureDir = async (dir: string) => {
         process.chdir(path.join(__dirname, "fixtures", dir));
+        try {
+            await exec("git diff --exit-code .");
+        } catch (e) {
+            throw new Error(
+                "Cannot run with dirty fixtures. git add your fixtures changes first."
+            );
+        }
         await exec("git clean -fd; git checkout .");
         return () => useFixtureDir(dir);
     };
@@ -23,9 +30,10 @@ describe("qla migration", () => {
     describe("running with no arguments", () => {
         let cleanup: Function;
         beforeAll(async () => {
-            cleanup = await useFixtureDir("migration");
+            cleanup = await useFixtureDir("no-args");
         });
         afterAll(() => cleanup());
+
         test("verify manifest", async () => {
             const manifest = await readManifest();
             // We run this first to force snapshots to be updated when fixtures change.
@@ -42,6 +50,20 @@ describe("qla migration", () => {
                       "version_number": 1,
                     }
                 `);
+                const migration = await fs.promises.readFile(
+                    manifest.migrations[0].js_file,
+                    "utf-8"
+                );
+                expect(migration).toMatchSnapshot();
             });
+        oclifTest
+            .command(["migration"])
+            .it(
+                "increments the migration number when adding another",
+                async (ctx) => {
+                    const manifest = await readManifest();
+                    expect(manifest.migrations).toMatchSnapshot();
+                }
+            );
     });
 });
