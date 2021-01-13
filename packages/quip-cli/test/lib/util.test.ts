db00bfe43611aa608abedaf3a33d312636d669b9
Jesse Ditson <jditson@quip.com>
2021-01-13 13:48:40 -0800

    Make util tests more cross platform

--- packages/quip-cli/test/lib/util.test.ts
+++ packages/quip-cli/test/lib/util.test.ts
@@ -3,28 +3,16 @@ import { readRecursive } from "../../src/lib/util";
 describe("readRecursive", () => {
     test("basics", async () => {
         const files = await readRecursive(
-            path.join(__dirname, "../fixtures/readdir"),
+            path.join(__dirname, "..", "fixtures", "readdir"),
             ""
         );
-        expect(files).toMatchInlineSnapshot(`
-            Array [
-              "test-dir/test-subdir/sub-sub-1",
-              "test-dir/test-subdir/sub-sub-2",
-              "test-dir/test-subfile",
-              "test-file",
-            ]
-        `);
+        expect(files).toMatchSnapshot();
     });
     test("skips", async () => {
         const files = await readRecursive(
-            path.join(__dirname, "../fixtures/readdir"),
+            path.join(__dirname, "..", "fixtures", "readdir"),
             "test-subdir"
         );
-        expect(files).toMatchInlineSnapshot(`
-            Array [
-              "test-dir/test-subfile",
-              "test-file",
-            ]
-        `);
+        expect(files).toMatchSnapshot();
     });
 });
