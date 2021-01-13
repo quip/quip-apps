e2ec16f478863e64b1da3367e2fe7988447d5d71
Jesse Ditson <jditson@quip.com>
2021-01-13 13:43:11 -0800

    wrong order

--- packages/quip-cli/test/snapshot-resolver-windows.js
+++ packages/quip-cli/test/snapshot-resolver-windows.js
@@ -1,17 +1,17 @@
 // Our snapshots are OS-dependent, and will be different on windows than mac.
-// This resolver simply adds _windows to the snapshots folder name.
+// This resolver simply adds _windows_ to the snapshots folder name.
 // We point to this config when we're in a windows environment via jest.config.js.
 
 module.exports = {
     // resolves from test to snapshot path
     resolveSnapshotPath: (testPath, snapshotExtension) =>
-        testPath.replace("__tests__", "__snapshots_windows__") +
+        testPath.replace("__snapshots_windows__", "__snapshots__") +
         snapshotExtension,
 
     // resolves from snapshot to test path
     resolveTestPath: (snapshotFilePath, snapshotExtension) =>
         snapshotFilePath
-            .replace("__snapshots_windows__", "__tests__")
+            .replace("__snapshots__", "__snapshots_windows__")
             .slice(0, -snapshotExtension.length),
 
     // Example test path, used for preflight consistency check of the implementation above
