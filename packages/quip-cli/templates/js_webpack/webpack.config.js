24464bf465adbe775068c2991f8b54aeb74720ba
Kevin Zhang <kevin.zhang@quip.com>
2021-01-22 14:28:59 -0800

    added headers to rest of examples

--- packages/quip-cli/templates/js_webpack/webpack.config.js
+++ packages/quip-cli/templates/js_webpack/webpack.config.js
@@ -32,5 +32,8 @@ module.exports = {
         contentBase: path.resolve(__dirname),
         port: 8888,
         inline: false,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
     },
 };
